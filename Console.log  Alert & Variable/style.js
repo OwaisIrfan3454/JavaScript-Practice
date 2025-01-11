383457617a55138fd52b29607beecd72b236a8b6
owaisirfan3454 <owaisk3454@gmail.com>
2025-01-12 00:56:13 +0500

    Console.log  Alert & Variable

---
+++ Console.log  Alert & Variable/style.js
@@ -0,0 +1,23 @@
+console.log("owais irfan");
+alert("owais");
+alert("abbasi");
+
+var myAge = "30";
+console.log(myAge);
+
+
+var my_firstName = "Owais";
+console.log(my_firstName);
+
+var _mylastName$ = "Irfan";
+console.log(_mylastName$);
+
+// var 123myAge = "27";Not Useable
+// console.log(123myAge);
+
+var $cityName = "Karachi";
+console.log($cityName);
+
+// var my@Email = "owaisk";Not Useable
+// console.log(my@Email);
+
