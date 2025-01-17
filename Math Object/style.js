32952a499e49e34947e947f81941ebbc8cea1457
owaisirfan3454 <owaisk3454@gmail.com>
2025-01-17 22:13:45 +0500

    Math Object

---
+++ Math Object/style.js
@@ -0,0 +1,22 @@
+const piValue = Math.PI;
+console.log (piValue);
+
+console.log (Math.abs(3));
+console.log (Math.abs(-3));
+
+const roundValue = Math.round(3.5);
+console.log (roundValue);
+
+const ceilValue = Math.ceil(3.1);
+console.log (ceilValue);
+
+const floorValue = Math.floor(3.9);
+console.log (floorValue);
+
+const truncValue = Math.trunc(3.9);
+console.log (truncValue);
+
+let squreRoot = Math.sqrt(25);
+console.log (squreRoot);
+
+console.log (Math.random().toFixed(3));
